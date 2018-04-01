eef5f4025a9b0cc2ab11d5099a14a1d4bb9e07a1
Daniel García Moreno <d_danigarcia@hotmail.es>
2018-04-01 23:46:14 +0200

    Puntos identificados por nombre. Ahora se muestran lineas entre puntos. De momento falta encontrar con algún evento que dibue las líneas a la vez que se mueve los puntos. De momento,  con el punto 1 se dibuja al soltar o pulsar.

--- srcPruebasSeleccionPuntos/gestionPuntosImagen.js
+++ srcPruebasSeleccionPuntos/gestionPuntosImagen.js
@@ -23,6 +23,13 @@ var punto2;
 var punto3;
 var punto4;
 
+var linea12;
+var linea23;
+var linea34;
+var linea41;
+
+var coordenadasDic = {};
+
 var imageObj;
 
 
@@ -33,7 +40,7 @@ function drawPuntos(){
 
 	//imagen de un punto
 	imageObj = new Image();
-	imageObj.src = "cuadrado.jpg";
+	imageObj.src = "cuadrado.png";
 
 
 	//los 4 puntos
@@ -77,16 +84,113 @@ function drawPuntos(){
 		name: "punto4"
 	});
 
+	punto1.cache();
+    punto1.drawHitFromCache();
+
+    punto1.on("mousemove", function () {
+	//alert(e.target.name() + " | x: " + e.target.x() + " " + "y: " + e.target.y());
+	
+	coordenadasDic["punto1"]["x"] = punto1.attrs.x;
+	coordenadasDic["punto1"]["y"] = punto1.attrs.y;
+
+
+	linea12.attrs.points = [coordenadasDic["punto1"]["x"] + imageObj.width/2, coordenadasDic["punto1"]["y"] + imageObj.height/2, punto2.attrs.x + imageObj.width/2, punto2.attrs.y + imageObj.height/2],
+
+	linea12.draw();
+
+});
+
+	coordenadasDic[punto1.name()] = {};
+	coordenadasDic[punto1.name()]["x"] = punto1.attrs.x;
+	coordenadasDic[punto1.name()]["y"] = punto1.attrs.y;
+	coordenadasDic[punto2.name()] = {};
+	coordenadasDic[punto2.name()]["x"] = punto1.attrs.x;
+	coordenadasDic[punto2.name()]["y"] = punto1.attrs.y;
+	coordenadasDic[punto3.name()] = {};
+	coordenadasDic[punto3.name()]["x"] = punto1.attrs.x;
+	coordenadasDic[punto3.name()]["y"] = punto1.attrs.y;
+	coordenadasDic[punto4.name()] = {};
+	coordenadasDic[punto4.name()]["x"] = punto1.attrs.x;
+	coordenadasDic[punto4.name()]["y"] = punto1.attrs.y;
+
+	 linea12 = new Konva.Line({
+		points: [punto1.attrs.x + imageObj.width/2, punto1.attrs.y + imageObj.height/2, punto2.attrs.x + imageObj.width/2, punto2.attrs.y + imageObj.height/2],
+		stroke: 'red',
+		strokeWidth: 5,
+		lineCap: 'round',
+		lineJoin: 'round',
+		/*
+		* line segments with a length of 29px with a gap
+		* of 20px followed by a line segment of 0.001px (a dot)
+		* followed by a gap of 20px
+		*/
+		dash: [29, 20, 0.001, 20]
+    });
+
+	linea23 = new Konva.Line({
+		points: [punto2.attrs.x + imageObj.width/2, punto2.attrs.y + imageObj.height/2, punto3.attrs.x + imageObj.width/2, punto3.attrs.y + imageObj.height/2],
+		stroke: 'red',
+		strokeWidth: 5,
+		lineCap: 'round',
+		lineJoin: 'round',
+		/*
+		* line segments with a length of 29px with a gap
+		* of 20px followed by a line segment of 0.001px (a dot)
+		* followed by a gap of 20px
+		*/
+		dash: [29, 20, 0.001, 20]
+    });
+
+	linea34 = new Konva.Line({
+		points: [punto3.attrs.x + imageObj.width/2, punto3.attrs.y + imageObj.height/2, punto4.attrs.x + imageObj.width/2, punto4.attrs.y + imageObj.height/2],
+		stroke: 'red',
+		strokeWidth: 5,
+		lineCap: 'round',
+		lineJoin: 'round',
+		/*
+		* line segments with a length of 29px with a gap
+		* of 20px followed by a line segment of 0.001px (a dot)
+		* followed by a gap of 20px
+		*/
+		dash: [29, 20, 0.001, 20]
+    });
+
+	linea41 = new Konva.Line({
+		points: [punto4.attrs.x + imageObj.width/2, punto4.attrs.y + imageObj.height/2, punto1.attrs.x + imageObj.width/2, punto1.attrs.y + imageObj.height/2],
+		stroke: 'red',
+		strokeWidth: 5,
+		lineCap: 'round',
+		lineJoin: 'round',
+		/*
+		* line segments with a length of 29px with a gap
+		* of 20px followed by a line segment of 0.001px (a dot)
+		* followed by a gap of 20px
+		*/
+		dash: [29, 20, 0.001, 20]
+    });
+
 	layer.add(punto1);
 	layer.add(punto2);
 	layer.add(punto3);
 	layer.add(punto4);
 
+	layer.add(linea12);
+	layer.add(linea23);
+	layer.add(linea34);
+	layer.add(linea41);
+
+
 	stage.add(layer);
 	stage.add(layer, dragLayer);
 };
 
+function actualizarDicCoordenadas(target){
+	coordenadasDic[target.name()]["x"] = target.x();
+	coordenadasDic[target.name()]["y"] = target.y();
+};
+
 //evento que ocurre tras dejar de arrastrar
 stage.on("dragend", function(e){
-	alert("x: " + e.target.x() + " " + "y: " + e.target.y());
-})
+	alert(e.target.name() + " | x: " + e.target.x() + " " + "y: " + e.target.y());
+	actualizarDicCoordenadas(e.target)
+});
