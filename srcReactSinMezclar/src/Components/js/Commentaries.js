e00fecf720c094173970ba972715cbfab3c7d9a6
Fernando Viñas Martins <fvm.inf@gmail.com>
2018-04-05 12:09:05 +0200

    Pequeño remodelado:
    -del módulo Commentaries para emplear estados en el muestreo de estos.
    
    -cambio de color en los bordes de los recuadros de los vídeos futuros.
    
    Signed-off-by: Fernando Viñas Martins <fvm.inf@gmail.com>

--- srcReactSinMezclar/src/Components/js/Commentaries.js
+++ srcReactSinMezclar/src/Components/js/Commentaries.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "../css/Commentaries.css";
+				
+const messagesTalker = ['¡Bien hecho!', 
+						'¡Perfecto!', 
+						 'Mola, ¿verdad?',
+						 'Activa la cámara web.',
+						 'Dale permisos a tu navegador.', 
+						 'No te preocupes.', 
+						 'Sólo tu app te verá.', 
+						 'No te grabamos.',
+						 'Mueve las cuatro esquinas.',
+						 'Colocalas en una superficie ladeada.',
+						 'El recuadro izquierdo se está transformando.',
+						 'El área seleccionada se está viendo de frente.'];
 
 class Commentaries extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {message: messagesTalker[3]};
+	}
+
 	render(){
 		return (
-			<ol id="Commentaries">
-				<li>Activa la cámara web. Permite al navegador que la use.</li>
-				<li>Busca en la pantalla algo que tenga cuatro esquinas.</li>
-				<li>El recuadro izquierdo se está transformando. El área seleccionada se está viendo de frente.</li>
-			</ol>
+			<div id="Commentaries-box">
+				<div id="Commentaries">
+					{this.state.message}
+				</div>
+			</div>
 		);
 	}
 }
