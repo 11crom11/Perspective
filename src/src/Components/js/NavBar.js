ee393de9ffc1011e740dee535eac545699d13e28
ivanClass <ivan.aguilera103@gmail.com>
2018-05-09 12:28:10 +0200

    - Añadido Reactstrap al proyecto - Modificado menú superior (en progreso) - Creada carpeta img, para almacenar las imágenes

--- src/src/Components/js/NavBar.js
+++ src/src/Components/js/NavBar.js
@@ -1,20 +1,72 @@
 import React from 'react';
 import '../css/NavBar.css';
+import iconoGitHub from '../img/github-icon.svg';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class NavBar extends React.Component {
-	render() {
-		return (
-		<nav className="navbar navbar-expand-lg navbar-dark primary-color fixed-top scrolling-navbar">
-	    	<div className="float-left">
-                <a className="button-collapse sidebar-toggle"><i className="fa fa-bars"></i></a>
-            </div>
-	    	<div className="text-centerer">
-	    		<h1 className="header-text">
-	    			Perspective
-	    		</h1>
-	    	</div>
-		</nav>);
-	}
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Perspective</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <a href="https://github.com/11crom11/Perspective"><img src={iconoGitHub} width="30" height="30"></img></a>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
